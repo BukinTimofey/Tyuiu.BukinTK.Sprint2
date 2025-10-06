853e9ec9230cf763ddd033ec96b773b97bab0ced
forengeit345 <timophey2907@gmail.com>
2025-10-06 20:01:33 +0500

    Add project files.

---
+++ Tyuiu.BukinTK.Sprint2.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task0.V4.Lib;
+
+namespace Tyuiu.BukinTK.Sprint2.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 333;
+            int y = 324;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint2.Task0.V4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task0.V4.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int x = 333;
+        int y = 324;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("X = " + x);
+        Console.WriteLine("Y = " + y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i<6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
