94dbcd1e63b01e9096526dd467efc28a2be1384d
forengeit345 <timophey2907@gmail.com>
2025-10-07 10:51:58 +0500

    Task1

---
+++ Tyuiu.BukinTK.Sprint2.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task1.V15.Lib;
+
+namespace Tyuiu.BukinTK.Sprint2.Task1.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int a = 415;
+            int b = 616;
+            int c = 134;
+            int d = 127;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, false, false, false, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint2.Task1.V15/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task1.V15.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int a = 415;
+        int b = 616;
+        int c = 134;
+        int d = 127;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("A = " + a);
+        Console.WriteLine("B = " + b);
+        Console.WriteLine("C = " + c);
+        Console.WriteLine("D = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
