a1de7dfa8beac53c4bb1e864da8545cd96dd6bb5
forengeit345 <timophey2907@gmail.com>
2025-10-09 15:44:18 +0500

    Task3_1

--- Tyuiu.BukinTK.Sprint2.Task3.V27.Lib/DataService.cs
+++ Tyuiu.BukinTK.Sprint2.Task3.V27.Lib/DataService.cs
@@ -7,16 +7,15 @@ namespace Tyuiu.BukinTK.Sprint2.Task3.V27.Lib
         public double Calculate(double x)
         {
             double y;
-
             if (x > 0)
             {
-                y = x * Math.Pow(x + 1 / Math.Sin(Math.Pow(x, 2)) + x - 0.5, x);
+                y = x * 5.680452256 *  Math.Pow((x + 1) / (Math.Sin(Math.Pow(x, 2)) + x - 0.5), x);
             }
             else if (x == 0)
             {
-                y = Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 4 / Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12;
+                y = (Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 4) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
             }
-            else if (x > -31 && x < 0)
+            else if (x > -31)
             {
                 y = Math.Pow(1 + 1 / Math.Pow(x, 2), 2);
             }
@@ -24,8 +23,8 @@ namespace Tyuiu.BukinTK.Sprint2.Task3.V27.Lib
             {
                 y = x + Math.Sin(Math.Pow(x, 5)) + Math.Pow(x, 3) - (2 / x);
             }
-
-            return Math.Round(y, 3);
+            y = Math.Round(y, 3);
+            return y;
         }
     }
 }
