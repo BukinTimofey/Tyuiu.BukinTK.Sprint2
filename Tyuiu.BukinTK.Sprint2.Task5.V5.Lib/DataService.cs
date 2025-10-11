3aeb46dfe990f53b7e20ad1a714eadbaf6eebf6c
forengeit345 <timophey2907@gmail.com>
2025-10-11 17:33:23 +0500

    Task5

---
+++ Tyuiu.BukinTK.Sprint2.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BukinTK.Sprint2.Task5.V5.Lib
+{
+    public class DataService : ISprint2Task5V5
+    {
+        public string FindCardValue(int value)
+        {
+            string cardName;
+            switch (value)
+            {
+                case 6: cardName = "шестерка"; break;
+                case 7: cardName = "семерка"; break;
+                case 8: cardName = "восьмерка"; break;
+                case 9: cardName = "девятка"; break;
+                case 10: cardName = "десятка"; break;
+                case 11: cardName = "валет"; break;
+                case 12: cardName = "дама"; break;
+                case 13: cardName = "король"; break;
+                case 14: cardName = "туз"; break;
+                default: cardName = "неизвестная карта"; break;
+            }
+            return cardName;
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task5.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер карты:");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        string res;
+
+        if ((value < 6) || (value > 14))
+        {
+            res = "Введено неверное значение!";
+        }
+        else
+        {
+            res = "Это карта " + ds.FindCardValue(value);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
