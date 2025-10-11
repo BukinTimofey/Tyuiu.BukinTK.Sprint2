c0627adeb93b8060426b84a2b2b0d0e5a27a5f13
forengeit345 <timophey2907@gmail.com>
2025-10-11 16:59:46 +0500

    Task4

---
+++ Tyuiu.BukinTK.Sprint2.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BukinTK.Sprint2.Task4.V26.Lib;
+
+namespace Tyuiu.BukinTK.Sprint2.Task4.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 1.0;
+            double res = ds.Calculate(x, y);
+            double wait = 12.0;
+
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 1.0;
+            double res = ds.Calculate(x, y);
+            double wait = 0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
